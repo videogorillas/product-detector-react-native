078260c0d869a89f1eeeeb058a3b21bdc0a776ce
day <marina.rusakova@gmail.com>
2017-01-31 15:12:37 +0200

    planogram report done

--- ProductDetector/product-detector.js
+++ ProductDetector/product-detector.js
@@ -7,7 +7,8 @@ import {
 	  Text,
 	  Image,
 	  TouchableHighlight,
-	  View
+	  View,
+      ListView,
 	} from 'react-native';
 
 import Spinner from 'react-native-loading-spinner-overlay';
@@ -94,7 +95,31 @@ class FramesComponent extends Component {
 class ProductComponent extends Component {
     constructor(props) {
 	    super(props);
-	    this.state = {report: false};
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        let planogram = this.getPlanogram(props.frames);
+        this.state = {
+          dataSource: ds.cloneWithRows(planogram)
+        };
+    }
+  
+    getPlanogram(frames) {
+      let planogram = [];
+      greenProducts.forEach((greenProduct, i, arr) => {
+        let p = planogram.find(x => x.label === greenProduct);
+        if (!p) {
+          p = {label: greenProduct, count: 0};
+          planogram.push(p);
+        }
+        frames.forEach((frame) => {
+          if (frame.label === p.label) {
+            p.count++;
+          }
+        });
+      });
+      planogram.sort((a, b) => {return b.count - a.count}); 
+        
+
+      return planogram;
     }
   
 	render() {
@@ -110,6 +135,7 @@ class ProductComponent extends Component {
        let screenH = Dimensions.get('window').height;
        let screenW = Dimensions.get('window').width;
        
+       // fit:
        if (screenW < imageW) {
          imageW = screenW;
          imageH = imageW / ar;
@@ -118,36 +144,51 @@ class ProductComponent extends Component {
          imageH = screenH;
          imageW = ar * imageH;
        }
-       
-//        // test:
-//        imageW = imageW/2;
-//        imageH = imageH/2;
-//        imageT = 25;
-      
+       // center horizontally:
        if (screenW > imageW) {
          imageL = (screenW - imageW)/2;
        }
-       console.log('>> ProductComponent render() ar=' + ar + ' screen ' + 
-                   screenW + 'x' + screenH + ' image ' + imageW + 'x' + imageH);
+       console.log('>> ProductComponent render() ar=' + ar + ' screen ' + screenW + 'x' + screenH + ' image ' + imageW + 'x' + imageH);
 
-       let style = {
-          position: 'absolute',
-          left: imageL,
-          top: imageT,
-          width: imageW, 
-          height: imageH};
+       let style = { position: 'absolute', left: imageL, top: imageT, width: imageW, height: imageH};
       
        let viewFlexDirection = (screenW > screenH ) ? 'row' : 'column';
       
-       let report = null;
+       let element;
        if (this.props.report) {
-         report = <View style={{position: 'absolute', left: 10, top: 10, width: screenW-30, height: screenH-40,
-                               backgroundColor: '#ffffff77', borderRadius: 5,
-                               justifyContent: 'space-between'}}>
-              <TouchableHighlight onPress={this.props.toggleReport}>
-                <Text style={{alignSelf: 'center'}}>CLOSE REPORT</Text>
-              </TouchableHighlight>
+         element = <View style={{position: 'absolute', left: 25, top: 10, width: screenW-50, height: screenH-20,
+                               backgroundColor: '#ffffff99', borderRadius: 5,
+                               justifyContent: 'space-between', flexDirection: 'column'}}>            
+               <Text style={{padding: 15, fontWeight: 'normal', fontSize: 20, color: 'black', textAlign: 'left'}}>Planogram Report</Text>
+               <View style={{flex: 1, paddingTop: 0, paddingBottom: 0}}>
+                 <ListView dataSource={this.state.dataSource} renderRow={this.renderRow}/>
+               </View>
+               <View style={{justifyContent: 'space-around', flexDirection: 'row'}}>
+                 <TouchableHighlight onPress={this.props.toggleReport} 
+                     activeOpacity={1} underlayColor={'#d3d3d355'} style={{borderRadius: 48}}>
+                     <Image source={require('./ic_close_white_24dp.png')} style={styles.ibutton} />
+                 </TouchableHighlight>
+                 <TouchableHighlight onPress={this.props.sendReport}
+                     activeOpacity={1} underlayColor={'#d3d3d355'} style={{borderRadius: 48}}>
+                     <Image source={require('./ic_send_white_24dp.png')} style={styles.ibutton} />
+                 </TouchableHighlight>
+               </View>
            </View>;
+       } else {
+         element = <View style={{flex: 1, justifyContent: 'space-between', flexDirection: viewFlexDirection}}>
+               <FramesComponent frames={this.props.frames} style={style}/>
+               <View style={{flex: 1}}/>
+               <View style={{justifyContent: 'space-around', flexDirection: 'row'}}>
+                 <TouchableHighlight onPress={this.props.clear} 
+                     activeOpacity={1} underlayColor={'#d3d3d355'} style={{borderRadius: 48}}>
+                     <Image source={require('./ic_close_white_24dp.png')} style={styles.ibutton} />
+                 </TouchableHighlight>
+                 <TouchableHighlight onPress={this.props.toggleReport}
+                     activeOpacity={1} underlayColor={'#d3d3d355'} style={{borderRadius: 48}}>
+                     <Image source={require('./ic_assignment_white_24dp.png')} style={styles.ibutton} />
+                 </TouchableHighlight>
+               </View>
+             </View>;
        }
          
 	   let component = 
@@ -155,22 +196,27 @@ class ProductComponent extends Component {
              <Image 
                  source={{uri: this.props.photo}}
                  style={style}/>
-             <FramesComponent frames={this.props.frames} style={style}/>
-             <View style={{flex: 1}}/>
-             <View style={{justifyContent: 'space-around', flexDirection: 'row'}}>
-               <TouchableHighlight onPress={this.props.clear} 
-                   activeOpacity={1} underlayColor={'#d3d3d355'} style={{borderRadius: 48}}>
-                   <Image source={require('./ic_close_white_24dp.png')} style={styles.ibutton} />
-               </TouchableHighlight>
-               <TouchableHighlight onPress={this.props.toggleReport}
-                   activeOpacity={1} underlayColor={'#d3d3d355'} style={{borderRadius: 48}}>
-                   <Image source={require('./ic_assignment_white_24dp.png')} style={styles.ibutton} />
-               </TouchableHighlight>
-             </View>
-             {report}
+             {element}
 	      </View>
 	    return component;
 	 }
+  
+     renderRow(row) {
+       let icon;
+       if (row.count > 0)
+         icon = require('./ic_check_black_24dp.png');
+       else
+         icon = require('./ic_warning_black_24dp.png');
+       return <View style={{paddingTop: 10, paddingBottom: 10, flexDirection: 'row'}}>
+         <View style={{flexDirection: 'column', justifyContent: 'space-around', paddingLeft: 15, paddingRight: 15}}>
+           <Image source={icon} style={{padding: 0}}/>
+         </View>
+         <View style={{flexDirection: 'column'}}>
+           <Text style={{padding: 3, fontWeight: 'normal', fontSize: 16, color: 'black'}}>{row.label}</Text>
+           <Text style={{padding: 3, fontSize: 12}}>{row.count > 0 ? row.count + ' positions': 'not found'}</Text>
+         </View>
+       </View>;
+     }
 }
 
 class ProductDetector extends Component {
@@ -178,31 +224,15 @@ class ProductDetector extends Component {
 	    super(props);
 	    this.state = {photo: undefined, photoW: 0, photoH: 0, frames: [], spinner: false, report: false};
 // 	    this.state = {
-// 	    	photo: 'file:///storage/emulated/0/DCIM/IMG_20170125_190854.jpg',
+//             photo: 'file:///storage/emulated/0/DCIM/IMG_20170131_120749.jpg', photoW: 720, photoH: 1280,
 // 	    	frames: [
-// 	    		{
-// 	    			id: 0,
-// 					ymin: 0, 
-// 					xmin: 0, 
-// 					width: 0.5, 
-// 					height: 0.5,
-// 					score: 1,
-// 					label: 'one'
-// 				},
-// 				{
-// 					id: 1,
-// 					ymin: 0, 
-// 					xmin: 0, 
-// 					width: 1, 
-// 					height: 1,
-// 					score: 0.5,
-// 					label: 'another'
-// 				}
+// 	    		{id: 0, ymin: 0.00, xmin: 0.00, width: 0.50, height: 0.50, score: 1.0, label: 'one'},
+// 				{id: 1, ymin: 0.00, xmin: 0.00, width: 1.00, height: 1.00, score: 0.5, label: 'another'},
+//                 {id: 2, ymin: 0.25, xmin: 0.25, width: 0.25, height: 0.20, score: 0.9, label: 'Miller'},
+//                 {id: 3, ymin: 0.65, xmin: 0.25, width: 0.55, height: 0.20, score: 0.8, label: 'smetana'},
 // 				],
-// 	    	spinner: false,
-//             photoW: 480,
-//             photoH: 800
-// 	    };
+// 	    	spinner: false, report: false};
+        
 	  }
 	
 	  render() {
@@ -219,11 +249,13 @@ class ProductDetector extends Component {
                           photo={this.state.photo} 
                           photoW={this.state.photoW}
                           photoH={this.state.photoH}
-                          frames={this.state.frames} 
+                          frames={this.state.frames}
+                          report={this.state.report}
                           clear={this.clear.bind(this)} 
                           setSpinner={this.setSpinner.bind(this)}
                           toggleReport={this.toggleReport.bind(this)}
-                          report={this.state.report}/>;
+                          sendReport={this.sendReport.bind(this)}
+                          />;
         }
         
         return (
@@ -239,8 +271,12 @@ class ProductDetector extends Component {
       }
   
       toggleReport() {
-          let report = this.state.report;
-          this.setState({report: !report});
+          this.setState({report: !this.state.report});
+      }
+  
+      sendReport() {
+          Alert.alert('Sent');
+          this.toggleReport();
       }
 	  
 	  setPhoto(photo) {
